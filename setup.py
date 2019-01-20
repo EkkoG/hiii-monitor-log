4cb4d14250cab783e254019b810645d91e5ec506
cielpy <beijiu572@gmail.com>
2019-01-20 13:42:23 +0800

    更新版本号

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='hiii-monitor-log',
-    version='0.0.1',
+    version='0.0.2',
     author='cielpy',
     author_email='beijiu572@gmail.com',
     url='https://github.com/cielpy/hiii-monitor-log',
